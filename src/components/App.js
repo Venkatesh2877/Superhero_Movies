f65ab8dac3ef690a0e652f8e9f532e89f0db08e4
Venkatesh2877 <venky2877@gmail.com>
2023-09-21 00:50:26 +0530

    added reducer,action

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,26 @@
+import React from "react";
+import { data } from "../data";
+import NavBar from "./NavBar";
+import MovieCard from "./MovieCard";
+
 function App() {
-  return <div className="App">Project setup</div>;
+  return (
+    <div className="App">
+      <NavBar />
+      <div className="main">
+        <div className="tabs">
+          <div className="tab">Movies</div>
+          <div className="tab">Favourite</div>
+        </div>
+
+        <div className="list">
+          {data.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
